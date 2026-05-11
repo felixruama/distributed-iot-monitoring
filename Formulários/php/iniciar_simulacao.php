69830eac7f0db63ea512a7a30e463a9a256e9aeb
Carolina Torres <carol2005torres@gmail.com>
2026-05-11 16:36:33 +0100

    Add Python script execution

--- Formulários/php/iniciar_simulacao.php
+++ Formulários/php/iniciar_simulacao.php
@@ -5,9 +5,21 @@ require_once('SPHandler.php');
 $id_simulacao = $_GET['id'];
 
 try {
+    // Script Python
+    $python_path = realpath(__DIR__ . '/../../ScriptsPython/MySQL(PC2)/cloudToMySQL.py');
+    $comando_python = "python " . escapeshellarg($python_path);
+    //espera acabar
+    exec($comando_python, $output_py, $status_py);
     $spManager->executeAction('SP_IniciarSimulacao', [$id_simulacao]);
+    // mazerun.exe
+    $mazerun_path = realpath(__DIR__ . '/../../Mazerun/mazerun.exe');
+    //abre o Mazerun.exe em segundo plano 
+    if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+        pclose(popen("start \"\" " . escapeshellarg($mazerun_path), "r"));
+    }
     header("Location: ../interface/dashboard.php?msg=simulacao_iniciada");
-} catch (PDOException $e) {
+} catch (Exception $e) {
+    // Se a SP falhar ou houver um erro, redireciona para a página de erro
     header("Location: ../interface/dashboard.php?erro=falha_iniciar");
 }
 exit();
